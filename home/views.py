af4d43a2a1a11e8373c80b42116a6d39e4d991d7
danial sadri <danielsadri01@gmail.com>
2023-09-10 20:28:00 +0330

    ...

--- home/views.py
+++ home/views.py
@@ -16,7 +16,7 @@ def home(request):
 #     todos = list(Todo.objects.order_by('priority').values('title', 'priority', 'is_done'))
 #     return JsonResponse({'todos': todos})
 
-@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+@api_view(['GET'])
 def todos_list(request: Request):
     todos = list(Todo.objects.order_by('priority').values('title', 'priority', 'is_done'))
     return Response({'todos': todos})
